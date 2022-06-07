305bb7eb8a7c3bc4e806a0a86757557d5a58c13c
Ivan7077 <105739809+Ivan7077@users.noreply.github.com>
2022-06-06 20:50:52 -0400

    Add files via upload

--- DrawC.java
+++ DrawC.java
@@ -1,16 +1,13 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-import java.awt.GridLayout;
-import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
 
-public class DrawC {
+public class DrawC extends JFrame implements ActionListener{
 
 	private JFrame framein;
-	private JPanel panel;
 	private JButton button1;
 	private JButton button2;
 	private JButton button3;
@@ -22,39 +19,42 @@ public class DrawC {
 		framein = new JFrame("Connect Four");
 	 
 	    button1 = new JButton();
-	    button1.addActionListener(e -> getGameMode(0);DrawG.main(););
 	    button1.setText("PvP");
 	    button1.setFocusable(false);
+	    button1.addActionListener(this);
+	    button1.setBounds(50,100,100,50);
 	        
 	    button2 = new JButton();
-	    button2.addActionListener(e -> getGameMode(1);DrawG.main(););
 	    button2.setText("PvE-Hard");
 	    button2.setFocusable(false);
-	    
+	    button2.addActionListener(this);
+	    button2.setBounds(150,100,100,50);
+
 	    button3 = new JButton();
-	    button3.addActionListener(e -> getGameMode(1);DrawG.main(););
 	    button3.setText("PvE-Medium");
 	    button3.setFocusable(false);
-
+	    button3.addActionListener(this);
+	    button3.setBounds(250,100,100,50);
+	    
 	    button4 = new JButton();
-	    button4.addActionListener(e -> getGameMode(1);DrawG.main(););
 	    button4.setText("PvE-Easy");
 	    button4.setFocusable(false);
+	    button4.addActionListener(this);
+	    button4.setBounds(450,100,100,50);
+
+	    
 
 	        
 	 	
-	 	panel = new JPanel();
-	 	panel.setBorder(BorderFactory.createEmptyBorder(50,50,50,50));
-	 	panel.setLayout(new GridLayout(0,1));
-	 	panel.add(button1);
-	 	panel.add(button2);
-	 	panel.add(button3);
-	 	panel.add(button4);
 
-		
-	 	framein.add(panel, BorderLayout.CENTER);
         framein.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         framein.pack();
+        framein.setSize(500,500);
+        framein.add(button1);
+        framein.add(button2);
+        framein.add(button3);
+        framein.add(button4);
+
         framein.setVisible(true);
         
 	 	
@@ -68,5 +68,24 @@ public class DrawC {
 		new DrawC();
 
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==button1) {
+			System.out.println("mode0");
+		}
+		else if(e.getSource()==button2) {
+			System.out.println("mode1");
+		}
+		else if(e.getSource()==button3) {
+			System.out.println("mode2");
+		}
+		else if(e.getSource()==button4) {
+			System.out.println("mode3");
+		}
+	
+}
+
+
 	
 }
